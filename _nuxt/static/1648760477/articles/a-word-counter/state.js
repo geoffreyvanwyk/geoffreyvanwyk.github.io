e766bd295277465cd6bca4feeb07c56ee5d52fe4
geoffreyvanwyk <geoffreyvanwyk@users.noreply.github.com>
2023-10-31 16:01:04 +0000

    deploy: geoffreyvanwyk/geoffreyvanwyk.dev@b54dc9353b6fa1b970eec0936df4bb8a9f02f074

--- _nuxt/static/1648760477/articles/a-word-counter/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648760477",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Fa-word-counter",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2f3f003d"}}}}(null));
